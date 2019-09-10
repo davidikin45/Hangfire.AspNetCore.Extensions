654a50047a6dd5f243b33e4e617ed0e096f1d24c
David Ikin <davidikin45@gmail.com>
2019-09-10 14:46:47 +0100

    changes

---
+++ src/Hangfire.AspNetCore.Extensions/HangfireEndpointRouteBuilderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Builder;
+#if NETCOREAPP3_0
+using Microsoft.AspNetCore.Routing;
+#endif
+using System;
+
+namespace Hangfire.AspNetCore.Extensions
+{
+    public static class HangfireEndpointRouteBuilderExtensions
+    {
+#if NETCOREAPP3_0
+        public static IEndpointConventionBuilder MapHangfireDashboard(this IEndpointRouteBuilder endpoints, string route = "/hangfire", Action<DashboardOptions> configAction = null, JobStorage storage = null)
+        {
+            var requestHandler = endpoints.CreateApplicationBuilder().UseHangfireDashboard(route, configAction, storage).Build();
+            return endpoints.Map(route + "/{**path}", requestHandler);
+        }
+#endif
+    }
+}
